c749e180b188c7eafa078a8a15284dd13c579be7
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-04-15 14:55:49 -0500

    completed project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,11 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+//  console.log( axios.get('https://api.github.com/users/slothwithannuzzi'))
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +30,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['slothwithannuzzi', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +51,64 @@ const followersArray = [];
       </div>
     </div>
 */
+
+const gitCard = (user) => {
+  const cardDiv = document.createElement('div')
+  const cardImg = document.createElement('img');
+  const infoDiv = document.createElement('div');
+  const nameHeader = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p');
+  const link = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  cardDiv.classList.add('card');
+  infoDiv.classList.add('card-info');
+  nameHeader.classList.add('name');
+  username.classList.add('username')
+  cardImg.src = user.avatar_url;
+  nameHeader.textContent = user.name;
+  username.textContent = user.login;
+  location.textContent = user.location;
+  link.textContent = user.html_url;
+  link.href = user.html_url;
+  followers.textContent = `Followers: ${user.followers}`
+  following.textContent = `Following: ${user.following}`
+  bio.textContent = `Bio: ${user.bio}`
+
+  cardDiv.appendChild(cardImg);
+  cardDiv.appendChild(infoDiv);
+
+  const infoArray = [nameHeader, username, location, profile, followers, following, bio];
+  infoArray.forEach(element => {
+    infoDiv.appendChild(element);
+  })
+
+  profile.appendChild(link);
+  console.log(cardDiv);
+
+  const cards = document.querySelector('div.cards');
+  cards.appendChild(cardDiv);
+}
+
+// a function to get the info from the username input, returns an error if no such username exists.
+const getUser = (username) => {
+return axios.get(`https://api.github.com/users/${username}`)
+  .then(({data}) => {
+      console.log(data)
+      gitCard(data);
+    })
+  .catch((err) => {
+    console.error('Invalid Username');
+  })
+
+}
+
+followersArray.forEach(element => {
+  getUser(element)
+})
 
 /*
   List of LS Instructors Github username's:
